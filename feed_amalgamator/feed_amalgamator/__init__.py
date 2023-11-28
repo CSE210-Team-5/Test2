f7ebfe1b0fea3c473022b2ee7e311a06d2684c69
Vancheeswaran Vaidyanathan <vigneshvanchy@gmail.com>
2023-11-28 11:01:47 -0800

    Reformatting Code Using Black

--- feed_amalgamator/feed_amalgamator/__init__.py
+++ feed_amalgamator/feed_amalgamator/__init__.py
@@ -4,17 +4,18 @@ from flask import Flask, redirect, url_for
 from . import db
 from . import auth, feed
 
+
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+        SECRET_KEY="dev",
+        DATABASE=os.path.join(app.instance_path, "flaskr.sqlite"),
     )
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
+        app.config.from_pyfile("config.py", silent=True)
     else:
         # load the test config if passed in
         app.config.from_mapping(test_config)
@@ -30,8 +31,8 @@ def create_app(test_config=None):
     app.register_blueprint(auth.bp)
     app.register_blueprint(feed.bp)
 
-    @app.route('/', methods=["GET"])
+    @app.route("/", methods=["GET"])
     def redirect_internal():
-        return redirect(url_for('auth.register'))
+        return redirect(url_for("auth.register"))
 
     return app
--- feed_amalgamator/feed_amalgamator/feed.py
+++ feed_amalgamator/feed_amalgamator/feed.py
@@ -2,27 +2,35 @@ import configparser
 import functools
 
 
-from flask import (Blueprint, flash, g, redirect, render_template, request,
-                   session, url_for)
+from flask import (
+    Blueprint,
+    flash,
+    g,
+    redirect,
+    render_template,
+    request,
+    session,
+    url_for,
+)
 from mastodon import Mastodon
 from werkzeug.security import check_password_hash, generate_password_hash
 from feed_amalgamator.db import get_db
 
-bp = Blueprint('feed', __name__, url_prefix='/feed')
+bp = Blueprint("feed", __name__, url_prefix="/feed")
 
 
-@bp.route('/home', methods=['GET'])
+@bp.route("/home", methods=["GET"])
 def feed_home():
-    if request.method == 'GET':
-        user_id = session['user_id']
+    if request.method == "GET":
+        user_id = session["user_id"]
         db = get_db()
         error = None
         user_servers = db.execute(
-            'SELECT * FROM user_server WHERE user_id = ?', (user_id,)
+            "SELECT * FROM user_server WHERE user_id = ?", (user_id,)
         ).fetchall()
 
         if user_servers is None:
-            error = 'Invalid User'
+            error = "Invalid User"
         timelines = []
         if error is None:
             parser = configparser.ConfigParser()
@@ -32,30 +40,38 @@ def feed_home():
             CLIENT_SECRET = client_dict["CLIENT_SECRET"]
             ACCESS_TOKEN = client_dict["ACCESS_TOKEN"]
             for user_server in user_servers:
-                domain = user_server['server']
-                token = user_server['token']
-                bot_m = Mastodon(client_id=CLIENT_ID, client_secret=CLIENT_SECRET,
-                                 access_token=ACCESS_TOKEN, api_base_url=domain)
+                domain = user_server["server"]
+                token = user_server["token"]
+                bot_m = Mastodon(
+                    client_id=CLIENT_ID,
+                    client_secret=CLIENT_SECRET,
+                    access_token=ACCESS_TOKEN,
+                    api_base_url=domain,
+                )
                 print(domain, token)
-                users_access_token = bot_m.log_in(code=token,
-                                                  redirect_uri='urn:ietf:wg:oauth:2.0:oob',
-                                                  scopes=["read", "write", "push"])
-                user_client = Mastodon(access_token=users_access_token, api_base_url=domain)
+                users_access_token = bot_m.log_in(
+                    code=token,
+                    redirect_uri="urn:ietf:wg:oauth:2.0:oob",
+                    scopes=["read", "write", "push"],
+                )
+                user_client = Mastodon(
+                    access_token=users_access_token, api_base_url=domain
+                )
                 timeline = user_client.timeline(timeline="home", limit=20)
                 timelines.append(timeline)
-            return render_template("feed/home.html", timelines=timelines )
+            return render_template("feed/home.html", timelines=timelines)
 
         flash(error)
 
-    return render_template('feed/home.html', timelines=None)
+    return render_template("feed/home.html", timelines=None)
 
 
-@bp.route('/add_server', methods=['GET', 'POST'])
+@bp.route("/add_server", methods=["GET", "POST"])
 def add_server():
-    if request.method == 'POST':
-        if 'domain' in request.form:
-            domain = request.form['domain']
-            session['domain'] = domain
+    if request.method == "POST":
+        if "domain" in request.form:
+            domain = request.form["domain"]
+            session["domain"] = domain
             parser = configparser.ConfigParser()
             parser.read("instance/client.ini")
             client_dict = parser["APP_TOKENS"]
@@ -63,23 +79,30 @@ def add_server():
             CLIENT_SECRET = client_dict["CLIENT_SECRET"]
             ACCESS_TOKEN = client_dict["ACCESS_TOKEN"]
             USERS_DOMAIN = "https://mastodon.social"
-            bot_m = Mastodon(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, access_token=ACCESS_TOKEN,
-                             api_base_url=USERS_DOMAIN)
-            url = bot_m.auth_request_url(redirect_uris="urn:ietf:wg:oauth:2.0:oob", scopes=["read", "write", "push"])
-            return render_template('feed/add_server.html', url=url, is_domain_set=True)
+            bot_m = Mastodon(
+                client_id=CLIENT_ID,
+                client_secret=CLIENT_SECRET,
+                access_token=ACCESS_TOKEN,
+                api_base_url=USERS_DOMAIN,
+            )
+            url = bot_m.auth_request_url(
+                redirect_uris="urn:ietf:wg:oauth:2.0:oob",
+                scopes=["read", "write", "push"],
+            )
+            return render_template("feed/add_server.html", url=url, is_domain_set=True)
 
-        elif 'token' in request.form:
-            auth_token = request.form['token']
-            user_id = session['user_id']
-            domain = session['domain']
+        elif "token" in request.form:
+            auth_token = request.form["token"]
+            user_id = session["user_id"]
+            domain = session["domain"]
             db = get_db()
             error = None
             if not auth_token:
-                error = 'Authorization Token in Required'
+                error = "Authorization Token in Required"
             elif not user_id:
-                error = 'Password is required.'
+                error = "Password is required."
             elif not domain:
-                error = 'domain is required'
+                error = "domain is required"
 
             if error is None:
                 try:
@@ -91,8 +114,8 @@ def add_server():
                 except db.IntegrityError:
                     error = f"Record already exists."
                 else:
-                    return redirect(url_for('feed.add_server', is_domain_set=False))
+                    return redirect(url_for("feed.add_server", is_domain_set=False))
 
             flash(error)
 
-    return render_template('feed/add_server.html', is_domain_set=False)
+    return render_template("feed/add_server.html", is_domain_set=False)
